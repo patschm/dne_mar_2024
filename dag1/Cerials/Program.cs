8616d20008742c99ff07477f364e3280bfc802ff
unknown <pschmidt@pasit.nl>
2024-03-11 12:37:55 +0100

    Day1

---
+++ dag1/Cerials/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Cerials;
+
+
+class Program
+{
+    static List<Person> people = new List<Person>();
+
+    static void Main(string[] args)
+    {
+        GenerateData(100);
+
+        //SerializeData(people);
+        //DeserializeData();
+        SerializeJsonData(people);
+
+        //ShowData(people);
+    }
+
+    private static void DeserializeData()
+    {
+         var fs = File.OpenRead(@"E:\Temp\people.xml");
+         var xml = new XmlSerializer(typeof(List<Person>));
+         List<Person>? pers = xml.Deserialize(fs) as List<Person>;
+         ShowData(pers!);
+    }
+
+    private static void SerializeData(List<Person> people)
+    {
+        var fs = File.OpenWrite(@"E:\Temp\people.xml");
+       XmlSerializer xml = new XmlSerializer(typeof(List<Person>));
+       xml.Serialize(fs, people);
+       fs.Flush();
+       fs.Close();
+    }
+
+    private static void SerializeJsonData(List<Person> people)
+    {
+        Stream fs = File.OpenWrite(@"E:\Temp\people.json");
+
+        var options = new JsonSerializerOptions();
+        options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+       JsonSerializer.Serialize(fs, people, options);
+       
+       fs.Flush();
+       fs.Close();
+    }
+    private static void ShowData(List<Person> people)
+    {
+        foreach (var p in people)
+        {
+            System.Console.WriteLine(p);
+        }
+    }
+
+    private static void GenerateData(int v)
+    {
+        people = new Bogus.Faker<Person>()
+            .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+            .RuleFor(p => p.LastName, f => f.Name.LastName())
+            .RuleFor(p => p.Age, f => f.Random.Int(0, 123))
+            .Generate(v)
+            .ToList();
+    }
+}
---
+++ dag1/Stromen/Program.cs
@@ -0,0 +1,110 @@
+﻿
+using System.IO.Compression;
+using System.Text;
+
+namespace Stromen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Directory
+        //File
+        //DirectoryInfo
+        //FileInfo
+
+        //BasicWrite();
+        //BasicRead();
+        //AdvancedWrite();
+        //AdvancedRead();
+        //CompressedWrite();
+        CompressedRead();
+    }
+
+    private static void BasicRead()
+    {
+        FileStream fs = File.OpenRead(@"E:\Temp\file1.txt");
+        byte[] emmertje = new byte[8];
+
+        while(fs.Read(emmertje, 0, emmertje.Length) > 0)
+        {
+            string txt = Encoding.UTF8.GetString(emmertje);
+            Console.Write(txt);
+            Array.Clear(emmertje, 0, emmertje.Length);
+        }
+    }
+
+    private static void BasicWrite()
+    {
+        FileInfo file = new FileInfo(@"E:\Temp\file1.txt");
+        if (file.Exists)
+            file.Delete();
+        FileStream fs = file.Create();
+        string txt = "Hello World";
+        for (int i = 0; i < 1000; i++)
+        {
+            byte[] data = Encoding.UTF8.GetBytes($"{txt} {i}\r\n");
+            fs.Write(data);
+        }
+        fs.Flush();
+        fs.Close();
+    }
+    private static void AdvancedRead()
+    {
+        FileStream fs = File.OpenRead(@"E:\Temp\file2.txt");
+
+        StreamReader reader = new StreamReader(fs);
+        string? line;
+        while((line = reader.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+    private static void AdvancedWrite()
+    {
+        FileInfo file = new FileInfo(@"E:\Temp\file2.txt");
+        if (file.Exists)
+            file.Delete();
+        FileStream fs = file.Create();
+        StreamWriter writer = new StreamWriter(fs);
+
+        string txt = "Hello World";
+        for (int i = 0; i < 1000; i++)
+        {
+            writer.WriteLine($"{txt} {i}");
+        }
+        writer.Flush();
+        fs.Flush();
+        fs.Close();
+    }
+    private static void CompressedRead()
+    {
+        FileStream fs = File.OpenRead(@"E:\Temp\file2.zip");
+        GZipStream zipper = new GZipStream(fs, CompressionMode.Decompress);
+
+        StreamReader reader = new StreamReader(zipper);
+        string? line;
+        while((line = reader.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+    private static void CompressedWrite()
+    {
+        FileInfo file = new FileInfo(@"E:\Temp\file2.zip");
+        if (file.Exists)
+            file.Delete();
+        FileStream fs = file.Create();
+        GZipStream zipper = new GZipStream(fs, CompressionMode.Compress);
+        StreamWriter writer = new StreamWriter(zipper);
+
+        string txt = "Hello World";
+        for (int i = 0; i < 1000; i++)
+        {
+            writer.WriteLine($"{txt} {i}");
+        }
+        writer.Flush();
+        fs.Flush();
+        fs.Close();
+    }
+}
